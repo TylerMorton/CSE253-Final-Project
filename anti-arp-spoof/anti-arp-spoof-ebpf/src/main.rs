ffde13eacc03989731de5898e3931731b6db73d9
Tyler Morton <tbmorton@ucsc.edu>
2024-12-12 02:11:06 -0800

    client table

--- anti-arp-spoof/anti-arp-spoof-ebpf/src/main.rs
+++ anti-arp-spoof/anti-arp-spoof-ebpf/src/main.rs
@@ -1,7 +1,14 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{bindings::{TC_ACT_OK, TC_ACT_SHOT}, macros::classifier, programs::TcContext};
+use aya_ebpf::{
+    bindings::{TC_ACT_OK, TC_ACT_SHOT},
+    macros::{
+        classifier,
+        map
+    }, programs::TcContext,
+    maps::Array
+};
 use aya_log_ebpf::{info, debug};
 
 use network_types::{
@@ -14,6 +21,19 @@ mod dhcp;
 use dhcp::DhcpHdr;
 
 pub const DHCP_MAGIC: u32 = 0x63825363;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct Client {
+    pub mac: [u8; 6],
+    pub ip: u32
+}
+
+#[map]
+pub static CLIENTS: Array<Client> = Array::with_max_entries(100, 0);
+
+#[map]
+pub static CLIENTS_SIZE: Array<u32> = Array::with_max_entries(1, 0);
 
 #[classifier]
 pub fn anti_arp_spoof(ctx: TcContext) -> i32 {
@@ -35,7 +55,7 @@ fn try_anti_arp_spoof(ctx: &TcContext) -> Result<i32, ()> {
         return Ok(TC_ACT_OK);
     }
     let udphdr: UdpHdr = ctx.load(EthHdr::LEN + Ipv4Hdr::LEN).map_err(|_| ())?;
-    if (u16::from_be(udphdr.source) != 67 || u16::from_be(udphdr.dest) != 68) {
+    if u16::from_be(udphdr.source) != 67 || u16::from_be(udphdr.dest) != 68 {
         debug!(ctx, "Not correct udp ports");
         return Ok(TC_ACT_OK);
     }
@@ -51,6 +71,15 @@ fn try_anti_arp_spoof(ctx: &TcContext) -> Result<i32, ()> {
     let mut chmac: [u8; 6] = [0; 6];
     chmac.copy_from_slice(&dhcphdr.chaddr[0..6]);
     debug!(ctx, "mac registered: {:X}{:X}{:X}{:X}{:X}{:X}", chmac[0], chmac[1], chmac[2], chmac[3], chmac[4], chmac[5]);
+    let clients_size = CLIENTS_SIZE.get_ptr_mut(0).ok_or(TC_ACT_OK).unwrap();
+    if unsafe {*clients_size} > 100 {
+        debug!(ctx, "client list full");
+        return Ok(TC_ACT_OK);
+    }
+    let client = unsafe {CLIENTS.get_ptr_mut(*clients_size).ok_or(TC_ACT_OK).unwrap()};
+    unsafe {(*client).mac = chmac};
+    unsafe {(*client).ip = dhcphdr.yiaddr}
+    //unsafe {(*client).mac.copy_from_slice(&dhc)}
     Ok(TC_ACT_OK)
 }
 
